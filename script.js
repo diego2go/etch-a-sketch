bce48e92fc5974d14bb6fec1427c99acbb97a062
diego2go <67561210+diego2go@users.noreply.github.com>
2023-03-22 18:33:01 -0600

    Add createGrid function

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ sliderContainer.appendChild(display);
 slider.oninput = function () {
     display.textContent = `${this.value} X ${this.value}`
 }
+// To set grid size dynamically
+const gridContainer = document.querySelector('.grid-container');
+let gridSize = slider.value;
+
+
+gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+function createGrid () {
+    for (let i = gridSize * gridSize; i > 0 ; i--) {
+        let div = document.createElement('div');
+        div.classList.add('item');
+        gridContainer.appendChild(div);
+    }
+}
